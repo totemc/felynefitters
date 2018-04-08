62d7631018ee5b28bc0ee436487bfc849019500c
Adrian <adrianr0120@gmail.com>
2018-04-08 17:27:03 -0400

    Switched to beta version of material-ui for grid

--- client/src/components/GearController.js
+++ client/src/components/GearController.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import GearHolder from './GearHolder';
 import Summary from './Summary';
+import GridList from 'material-ui/GridList';
 
 class GearController extends React.Component{
 
   render (){
     return(
         <div>
-
+        	<GridList>
+        		asdfsa
+        	</GridList>
         </div>
     );
   }
